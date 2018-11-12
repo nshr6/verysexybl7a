5dc407e792bba40137e7b8ec2cd479d22e7bfc4d
nshr6 <42012625+nshr6@users.noreply.github.com>
2018-11-12 10:33:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,110 +10,69 @@ const spammer8 = new Discord.Client();
 const spammer9 = new Discord.Client();
 const prefix = "!!"
 spammer1.on('ready', () => {
-    console.log(`Logged in as ${spammer1.user.tag}!`);
-  var server = "509609934472871936";
-  var channel = "509609934472871938";
-  var spamwords = ["spapspadpsapdmsdpmsofjdisahfudoshfaudsigfiudasgfiydDFSFASDF", "FDASFAOSDJFIODSAFCUYN78NFY87FDSIOFDSUA89FCY78EWFIOUWE9F", "a7a al 4ebe4 da s3d3sa5d6s74dfsafads", 
-  "كسمك يا جلال كسشمينملبيل كسم جلال احا يسطى", "GFDGFDGSDFGFDS GFDSG FDGK,[SDFP,G بيسحيجحلنبجيسحل506546 8954698-0لخني", "كطبينخحلتبيهسختلع8هعةلبيس89ةغل7ف", 
-  "874fads89f74d98sf74d8s94fg798d4g98fd4 g98f4dsg6fd4sg98 4sdfg84 fsd", "a7a al 4eb4 da3 a7a da kan be soba3 a7a da kan gded", "ksomk al cc da ebn klb 3rs ws5 4rmoot", 
-  `‏نقدم لك دعوة لسموك الراقيَ . \n
-  ‏لنحظى بجمال حضورك وتواجدك\n
-  •بتنـورونـا الجميع :wink:\n
-  •اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:\n
-  •عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:\n
-  •واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.\n
-  شرفنا يا عسل:wink: :kissing_heart:\n`, `fuck , you pr \n credit miner here \n will fuck pro bot`];
-  let timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  setInterval(() => {
-  let spam = `${spamwords[Math.floor(Math.random() * spamwords.length)]}`;
-  spammer1.guilds.get(server).channels.get(channel).send(`${spam}`)
-  },`${timer}`);
-  setInterval(() => {
+ let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        spammer1.guilds.get(server).channels.get(channel).send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+        setInterval(() => {
   spammer1.guilds.get(server).channels.get(channel).send("#daily")
   },700000);
-});
+      });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 spammer2.on('ready', () => {
-    console.log(`Logged in as ${spammer2.user.tag}!`);
-  var server = "509609934472871936";
-  var channel = "509609934472871938";
-  var spamwords = [`السلام عليكم 
-فتوكاتى العزيزات قريت الموضوع ده وعجبنى لعضو اسمه مونو
-فى احد المنتديات وحبيت انقله لكم هنا دى بعض الشتايم المصريه ومعانيها ومايروحش بالك لبعيد ههههه
-ويلا نستمتع بالموضوع
-`, `ولفظة "عبيط" هى لفظة مصرية قدي\nمة مركبة من عا+ بيط, فإذا عرفنا أن لفظة "عا" تعنى حمار فى الهيروغليفية ، وأن لفظة "بِيت" تعنى شخصية ، فيكون معنى الكلمتان \nمعاً هو حمار الشخصية, وتعضدنا فى ذلك اللغة الإنجليزية حيت تُ\nتَرجم القواميس اللفظة donkey بمعنى حمار ، شخص غبى.
-`, `ويقول البعض "هلزقك بالقلم" فهى ترادف المعنى الأول تماماً. وأصل كلم`, `English swear words are recognized all around \nthe world, used in movies, literature, and TV shows.
-
-`, `We certainly don’t want to encourage you to use these English swear words at other people. But understanding what these words mean will at least help you be aware in case other people use it against you!
-`, `Adding the word ‘you’ means you’re directing the offense onto someone else. It’s often used as a joke or when you’re angry at someone else.
-
-`, `Choad is just another word for penis and can be used similarly to the way the word ‘dick’ is used in America.
-
-`];
-  let timer = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000
-  setInterval(() => {
-  let spam = `${spamwords[Math.floor(Math.random() * spamwords.length)]}`;
-  spammer2.guilds.get(server).channels.get(channel).send(`${spam}`)
-  },`${timer}`);
-  setInterval(() => {
+ let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        spammer2.guilds.get(server).channels.get(channel).send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+        setInterval(() => {
   spammer2.guilds.get(server).channels.get(channel).send("#daily")
   },700000);
-});
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      });//////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 spammer3.on('ready', () => {
-    console.log(`Logged in as ${spammer3.user.tag}!`);
-  var server = "509609934472871936";
-  var channel = "509609934472871938";
-  var spamwords = [`السلام عليكم 
-فتوكاتى العزيزات قريت الموضوع ده وعجبنى لعضو اسمه مونو
-فى احد المنتديات وحبيت انقله لكم هنا دى بعض الشتايم المصريه ومعانيها ومايروحش بالك لبعيد ههههه
-ويلا نستمتع بالموضوع`, `We certainly don’t want to encourage you to use these English swear words at other people. But understanding what these words mean will at least help you be aware in case other people use it against you!
-`, `Adding the word ‘you’ means you’re directing the offense onto someone else. It’s often used as a joke or when you’re angry at someone else.
-
-`, `Choad is just another word for penis and can be used similarly to the way the word ‘dick’ is used in America.
-
-`, "كسمك يا جلال كسشمينملبيل كسم جلال احا يسطى", "GFDGFDGSDFGFDS GFDSG FDGK,[SDFP,G بيسحيجحلنبجيسحل506546 8954698-0لخني", "كطبينخحلتبيهسختلع8هعةلبيس89ةغل7ف", 
-  "874fads89f74d98sf74d8s94fg798d4g98fd4 g98f4dsg6fd4sg98 4sdfg84 fsd", "a7a al 4eb4 da3 a7a da kan be soba3 a7a da kan gded", "ksomk al cc da ebn klb 3rs ws5 4rmoot", 
-];
-  let timer = 5000;
-  setInterval(() => {
-  let spam = `${spamwords[Math.floor(Math.random() * spamwords.length)]}`;
-  spammer3.guilds.get(server).channels.get(channel).send(`${spam}`)
-  },`${timer}`);
-  setInterval(() => {
+ let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        spammer3.guilds.get(server).channels.get(channel).send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+        setInterval(() => {
   spammer3.guilds.get(server).channels.get(channel).send("#daily")
   },700000);
-});
+      });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 spammer4.on('ready', () => {
-    console.log(`Logged in as ${spammer4.user.tag}!`);
-  var server = "509609934472871936";
-  var channel = "509609934472871938";
-  var spamwords = ["spapspadpsapdmsdpmsofjdisahfudoshfaudsigfiudasgfiydDFSFASDF", "FDASFAOSDJFIODSAFCUYN78NFY87FDSIOFDSUA89FCY78EWFIOUWE9F", "a7a al 4ebe4 da s3d3sa5d6s74dfsafads", 
-  "كسمك يا جلال كسشمينملبيل كسم جلال احا يسطى", "GFDGFDGSDFGFDS GFDSG FDGK,[SDFP,G بيسحيجحلنبجيسحل506546 8954698-0لخني", "كطبينخحلتبيهسختلع8هعةلبيس89ةغل7ف", 
-  "874fads89f74d98sf74d8s94fg798d4g98fd4 g98f4dsg6fd4sg98 4sdfg84 fsd", "a7a al 4eb4 da3 a7a da kan be soba3 a7a da kan gded", "ksomk al cc da ebn klb 3rs ws5 4rmoot", 
-  `‏نقدم لك دعوة لسموك الراقيَ . \n
-  ‏لنحظى بجمال حضورك وتواجدك\n
-  •بتنـورونـا الجميع :wink:\n
-  •اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:\n
-  •عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:\n
-  •واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.\n
-  شرفنا يا عسل:wink: :kissing_heart:\n`, `fuck , you pr \n credit miner here \n will fuck pro bot`];
-  let timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  setInterval(() => {
-  let spam = `${spamwords[Math.floor(Math.random() * spamwords.length)]}`;
-  spammer4.guilds.get(server).channels.get(channel).send(`${spam}`)
-  },`${timer}`);
-  setInterval(() => {
+ let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        spammer4.guilds.get(server).channels.get(channel).send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+        setInterval(() => {
   spammer4.guilds.get(server).channels.get(channel).send("#daily")
   },700000);
-});
+      });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
